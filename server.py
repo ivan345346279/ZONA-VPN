897b2c8f926c3acd2d810f09cc0b3bf68ce27114
Saffy <youblogivan@gmail.com>
2026-03-28 10:33:00 +0300

    Update server.py

--- server.py
+++ server.py
@@ -263,4 +263,6 @@ if __name__ == "__main__":
     print(f"🔑 Пароль    →  {ADMIN_PASSWORD}")
     print("⚠️  Telegram demo" if demo else "✅  Telegram widget активен")
     print("="*50)
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
